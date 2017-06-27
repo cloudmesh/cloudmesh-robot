7929540a3705080f02a26c3fda9da6f104b6c65f
Gregor von Laszewski <laszewski@gmail.com>
2017-06-27 07:10:23 -0400

    fix:dev: code inspection position.py

--- cloudmesh/robot/positions.py
+++ cloudmesh/robot/positions.py
@@ -1,36 +1,38 @@
-import sys
-import time
 # import requests
-from pprint import pprint
-from cloudmesh.common.Printer import Printer
 import random
-import time
+import datetime
+
+from cloudmesh.common.Printer import Printer
+
+
 #
 # BUG marvelmind can not be found
 #
 # from marvelmindcoordinates import MarvelmindHedge
 
-class BeaconSimulator():
+class BeaconSimulator:
 
-    def get (addr):
+    # noinspection PyMethodMayBeStatic
+    def get(self, addr):
         x = random.random() * 100.0
         y = random.random() * 100.0
-        return (addr, x, y, time.now())
+        return addr, x, y, datetime.now()
+
 
 class PositioningSystem(object):
-
     def __init__(self, tty='/dev/tty.usbmodemFD121'):
         self.tty = tty
         self.beacon = {}
         self.id = 0
         self.count = {
             'mobile': 0,
-            'stationary' :0,
+            'stationary': 0,
         }
-#       self.sensors =  MarvelmindHedge(self.tty)
+
+    #       self.sensors =  MarvelmindHedge(self.tty)
 
     def pos(self, name):
-        return self.beacon[name]['x'],self.beacon[name]['y']
+        return self.beacon[name]['x'], self.beacon[name]['y']
         pass
 
     def export(self, filename):
@@ -50,7 +52,7 @@ class PositioningSystem(object):
             'addr': addr,
             'x': 0.0,
             'y': 0.0,
-            't': time.now()
+            't': datetime.now(),
             'id': self.id
         }
         self.count[kind] = self.count[kind] + 1
@@ -63,29 +65,29 @@ class PositioningSystem(object):
         return None
 
     def reset(self, kind):
-        t = time.now()
+        t = datetime.now()
         for name in self.beacon:
             if self.beacon[name]['kind'] == kind:
-                self.set(0.0,0.0, t)
+                self.set(0.0, 0.0, t)
 
     def set(self, name, x, y, t=None):
         self.beacon[name]['x'] = x
         self.beacon[name]['y'] = y
-        if t == None:
-            self.beacon[name][t] = time.now()
+        if t is None:
+            self.beacon[name][t] = datetime.now()
         else:
             self.beacon[name][t] = t
 
-    def info(self, dt=3.0, kind='mobile'):
-        print ('Positioning System Information')
-        print ('Stationary Beacons:', self.count['statinary'])
-        print ('Mobile Beacons:    ', self.count['mobile'])
+    def info(self, dt=3.0):
+        print('Positioning System Information')
+        print('Stationary Beacons:', self.count['statinary'])
+        print('Mobile Beacons:    ', self.count['mobile'])
         # checks also if beacon was active and prints that
         data = dict(self.beacon)
         for name in data:
             # if time now - dt > time:
             data[name]['active'] = True
-            #else
+            # else
             # data[name]['active'] = False
 
         print(Printer.dict(positions.beacon, order=['id', 'name', 'kind', 'active', 'x', 'y', 'addr']))
@@ -101,13 +103,12 @@ class PositioningSystem(object):
     def __str__(self):
         return Printer.dict(positions.beacon, order=['id', 'name', 'kind', 'x', 'y', 'addr'])
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     positions = PositioningSystem()
 
-    print (positions.tty)
-    print (positions.beacon)
-
+    print(positions.tty)
+    print(positions.beacon)
 
     positions.register("Hedge 1", "robi15")
     positions.register("Hedge 3", "robi16")
@@ -119,5 +120,3 @@ if __name__ == "__main__":
     print(Printer.dict(positions.beacon, order=['id', 'name', 'kind', 'x', 'y', 'addr']))
 
     positions.info()
-
-
