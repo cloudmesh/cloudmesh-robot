eb61865868ee43a3e0fa494ac5d7ce1fc62e8745
Gregor von Laszewski <laszewski@gmail.com>
2017-06-27 06:51:49 -0400

    chg:usr: move position.py to cloudmesh/robot

--- cloudmesh/robot/positions.py
+++ cloudmesh/robot/positions.py
@@ -23,6 +23,10 @@ class PositioningSystem(object):
         self.tty = tty
         self.beacon = {}
         self.id = 0
+        self.count = {
+            'mobile': 0,
+            'stationary' :0,
+        }
 #       self.sensors =  MarvelmindHedge(self.tty)
 
     def pos(self, name):
@@ -46,8 +50,10 @@ class PositioningSystem(object):
             'addr': addr,
             'x': 0.0,
             'y': 0.0,
+            't': time.now()
             'id': self.id
         }
+        self.count[kind] = self.count[kind] + 1
         self.id = self.id + 1
 
     def lookup(self, addr):
@@ -56,32 +62,50 @@ class PositioningSystem(object):
                 return self.beacon[i]
         return None
 
-    def reset(self):
+    def reset(self, kind):
+        t = time.now()
         for name in self.beacon:
-            self.beacon[name]['x'] = 0.0
-            self.beacon[name]['y'] = 0.0
+            if self.beacon[name]['kind'] == kind:
+                self.set(0.0,0.0, t)
 
-    def info(self, kind='mobile'):
-        # mobile, all, stationarty
+    def set(self, name, x, y, t=None):
+        self.beacon[name]['x'] = x
+        self.beacon[name]['y'] = y
+        if t == None:
+            self.beacon[name][t] = time.now()
+        else:
+            self.beacon[name][t] = t
+
+    def info(self, dt=3.0, kind='mobile'):
+        print ('Positioning System Information')
+        print ('Stationary Beacons:', self.count['statinary'])
+        print ('Mobile Beacons:    ', self.count['mobile'])
+        # checks also if beacon was active and prints that
+        data = dict(self.beacon)
+        for name in data:
+            # if time now - dt > time:
+            data[name]['active'] = True
+            #else
+            # data[name]['active'] = False
+
+        print(Printer.dict(positions.beacon, order=['id', 'name', 'kind', 'active', 'x', 'y', 'addr']))
+
         pass
 
-        # dependent on type returns info about the beacons
-        # not sure how to implement yet
+    def count(self, kind='mobile'):
+        return self.count[kind]
 
-    def count(self, type='mobile'):
-        # retusn number of beacons found
-        pass
+    def set_count(self, n, kind='mobile'):
+        self.count[kind] = n
 
-    def set_count(self, type='mobile'):
-        # instead of probing the beacons hard code the set count
-        # so we can figure out if we have power issue
-        pass
-
+    def __str__(self):
+        return Printer.dict(positions.beacon, order=['id', 'name', 'kind', 'x', 'y', 'addr'])
 
 positions = PositioningSystem()
 
 print (positions.tty)
 print (positions.beacon)
+
 
 positions.register("Hedge 1", "robi15")
 positions.register("Hedge 3", "robi16")
@@ -92,34 +116,6 @@ positions.export('g.txt')
 
 print(Printer.dict(positions.beacon, order=['id', 'name', 'kind', 'x', 'y', 'addr']))
 
+positions.info()
 
 
-sys.exit(0)
-
-
-"""
-# assume start at 0 0
-move 0 1
-move 1 1
-move 5 5
-"""
-
-
-# def move(ip, direction, epsilon=0.0001):
-#    if direction == 'left':
-#        payload = {'LEFT': 'ON'}
-#    elif direction == 'right':
-#        payload = {'RIGHT': 'ON'}
-#    elif  direction == 'stop':
-#        payload = {'STOP': 'ON'}
-#    elif direction == 'forward':
-#        payload = {'FORWARD': 'ON'}
-#    elif direction == 'backward':
-#        payload = {'BACK': 'ON'}
-#    else:
-#        print ("ERROR: unkown direction:", direction)
-#    try:
-#        headers = {'User-Agent': "Car"}
-#        r = requests.get('http://' + ip, headers=headers, params=payload)
-#    except Exception as e:
-#        print (type(e), e)
