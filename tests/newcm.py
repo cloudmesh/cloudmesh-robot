603c315e5e71c5fe2d363840a0b1c77325857c09
Local Administrator <localadmin@ISELAB-MAC-01.local>
2017-06-30 16:20:40 -0400

    new:dev: updated Servo and import from cm

--- tests/newcm.py
+++ tests/newcm.py
@@ -8,6 +8,15 @@ import math
 
 
 ##############################################
+# PIN MANAGEMENT
+##############################################
+
+from cm import p
+from cm import pin_id
+from cm import mac
+
+
+##############################################
 # MOTOR MANAGEMENT
 ##############################################
 
@@ -49,109 +58,16 @@ class Motor(object):
 # LED
 ##############################################
 
-class LED(object):
-    # pin = 2
-    def __init__(self, pin):
-        """
-        defne an LED on a given pin
-        :param pin: the number of the pin
-        """
-        self.pin = pin
-        self.light = machine.Pin(self.pin, machine.Pin.OUT)
+from cm import LED
 
-    def on(self):
-        """
-        switch on the LED
-        """
-
-        self.light.low()
-
-    def off(self):
-        """
-        switch the LED off
-        """
-        self.light.high()
-
-    def blink(self, n, dt=0.1):
-        """
-        make the LED blink
-        :param n: number of blinks
-        :param dt: time delay between on and off
-        :return:
-        """
-        for i in range(0, n):
-            self.on()
-            utime.sleep(dt)
-            self.off()
-            utime.sleep(dt)
-            self.on()
-
-##############################################
-# PIN MANAGEMENT
-##############################################
-
-def p(name):
-    """
-    return the pin from the pin name
-    :param name: the pin name D1 ... D8
-    :return:
-    """
-    mapping = {
-        "D0": 16,
-        "D1": 5,
-        "D2": 4,
-        "D3": 0,
-        "D4": 2,
-        "D5": 14,
-        "D6": 12,
-        "D7": 13,
-        "D8": 15,
-        "D9": 3,
-        "D10": 1,
-    }
-
-    if name in mapping:
-        return mapping[name]
-    else:
-        return 2
-
-
-def pin_id(pin):
-    if type(pin) == str:
-        return p(pin)
-    elif type(pin) == int:
-        return pin
-
-
-def mac():
-    return ubinascii.hexlify(network.WLAN().config('mac'), ':').decode()
 
 ##############################################
 # FILE MANAGEMENT
 ##############################################
 
-def cat(filename):
-    """
-    prints the content of a file
-    :param filename: the filename
-    """
-    f = open(filename)
-    data = f.read()
-    print(data)
-    f.close()
-
-
-def ls():
-    print('\n'.join(os.listdir(".")))
-
-
-def clean():
-    for file in ['boot.py', 'webrepl_cfg.py']:
-        try:
-            os.remove("boot.py")
-        except Exception as e:
-            print ("File does not exist", file)
-
+from cm import cat
+from cm import ls
+from cm import clean
 
 ##############################################
 # SERVO
@@ -208,106 +124,51 @@ class Servo(object):
 # NETWORK MANAGEMENT
 ##############################################
 
-def get_attributes(filename):
-    f = open(filename)
-    contents = f.read()
-    f.close()
-    contents.replace("\r\n", "\n")
-
-    attributes = {}
-    lines = contents.split("\n")
-    for line in lines:
-        if ":" in line:
-            attribute, value = line.split(":")
-            attributes[attribute.strip()] = value.strip()
-
-    return attributes
-
-
-def connect(filename='credentials.txt'):
-    """
-    connects to the network while using the credentials
-    :return: the network information
-    """
-    print('starting network ...')
-
-    credentials = get_attributes(filename)
-    print(credentials)
-
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(credentials['ssid'], credentials['password'])
-        while not sta_if.isconnected():
-            pass
-    print('connection ok')
-
-    return sta_if.ifconfig()
-
-
-def net(ssid=None, password=None, username='gregor'):
-    if ssid is None and password is None:
-        cat("credentials.txt")
-    else:
-        d = {
-            'ssid': ssid,
-            'password': password,
-            'username': username
-        }
-        with open('credentials.txt', 'w') as f:
-            for i in ['ssid', 'password']:
-                f.write(i + ": " + d[i] + "\n")
-            for i in ['username']:
-                f.write(i + ": " + d[i])
-
-
-def ap(filename='credentials.txt'):
-
-    credentials = get_attributes(filename)
-    print(credentials)
-
-    print("start ap")
-
-    sta_if = network.WLAN(network.STA_IF)
-    ap_if = network.WLAN(network.AP_IF)
-
-    sta_if.active()
-    ap_if.active()
-    ap_if.ifconfig()
-
-    sta_if.connect(credentials['ssid'], credentials['password'])
-    print(sta_if)
-
-    print(sta_if.ifconfig())
-    print(ap_if.ifconfig())
+from cm import get_attributes
+from cm import connect
+from cm import net
+from cm import ap
 
 
 ##############################################
 # WEBPAGE MANAGEMENT
 ##############################################
 
-def feedback(conn, html):
-    response = ''.join(html.split('\n')[1:])
+from cm import feedback
 
-    response_headers = {
-        'Content-Type': 'text/html; encoding=utf8',
-        'Content-Length': len(response),
-        'Connection': 'close',
-    }
-    response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.items())
+##############################################
+# RPM MANAGEMENT
+##############################################
 
-    # Reply as HTTP/1.1 server, saying "HTTP OK" (code 200).
-    response_proto = 'HTTP/1.1'
-    response_status = '200'
-    response_status_text = 'OK'  # this can be random
 
-    # sending all this stuff
-    conn.send('%s %s %s' % (response_proto, response_status, response_status_text))
-    conn.send(response_headers_raw)
-    conn.send('\n')  # to separate headers from body
+class SpeedMeter(object):
 
-    conn.send(response)
+    def __init__(self, pin):
+        self.pin = machine.Pin(pin, machine.Pin.IN)
+        self.status = self.pin.value(self.pin)
+        self.counter = 0
+
+    def update(self):
+        self.status = self.pin.value(self.pin)
+
+    def get(self):
+        turn = True
+        t0 = utime.ticks_ms()
+        delta_t = 0
+        count_start = self.counter
+        while delta_t < 100:
+            self.update()
+            if self.status == 1 and turn:
+                turn = False
+                self.counter += 1
+            elif self.status == 0:
+                turn = True
+                delta_t = utime.ticks_diff(utime.ticks_ms(), t0)
+        count_dt = self.counter - count_start
+        return count_dt
+
+    def rpm(self):
+        return (self.get() / 20) * 300
 
 
 
