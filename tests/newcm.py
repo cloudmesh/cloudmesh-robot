f138be9cd4401629e454b941daed58b9fa91f89d
Jon Montgomery <jonmontgomery62@gmail.com>
2017-07-06 16:37:39 -0400

    new:dev: new Car class that combines robot parts

--- tests/newcm.py
+++ tests/newcm.py
@@ -183,6 +183,52 @@ class SpeedMeter(object):
                 turn = True
 
 
+##############################################
+# RPM MANAGEMENT
+##############################################
+
+
+class Car(object):
+
+    def __init__(self,
+                 leftmotor,
+                 rightmotor,
+                 leftspeedmeter,
+                 rightspeedmeter
+                 ):
+        """
+        Consolidates robot parts to one object
+        :param leftmotor: Motor
+        :param rightmotor: Motor
+        :param leftspeedmeter: SpeedMeter
+        :param rightspeedmeter: SpeedMeter
+        """
+        self.left = leftmotor
+        self.right = rightmotor
+        self.sml = leftspeedmeter
+        self.smr = rightspeedmeter
+
+    def turn_angle(self, angle):
+        """
+        Turns robot given angle. Turn left with negative. Turn right with positive.
+        :param angle: Number (angle in degrees)
+        """
+        ticks = abs(((((angle * math.pi) / 180) * 13.6) / (6.7 * math.pi)) * 20)
+        if angle < 0:
+            self.right.set(800)
+            self.right.forward()
+            self.smr.wait_ticks(ticks)
+            self.right.stop()
+            self.right.set(1023)
+        elif angle > 0:
+            self.left.set(800)
+            self.left.forward()
+            self.sml.wait_ticks(ticks)
+            self.left.stop()
+            self.left.set(1023)
+
+
+
 
 
 
