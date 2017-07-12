17cc8a0996346f6db693287ce679f97052cbd437
Jon <jon@149-161-153-59.dhcp-bl.indiana.edu>
2017-07-12 15:18:27 -0400

    new:dev: first attempt of autonomous move

--- dev/move-algorithm.py
+++ dev/move-algorithm.py
@@ -1,8 +1,7 @@
 import cm3
+import requests
+import time
 from marvelmind.marvelmind import MarvelmindHedge
-
-#left = cm.Motor("left")
-#right = cm.Motor("right")
 
 
 filename = "move-algorithm.txt"
@@ -22,20 +21,82 @@ ips = ['10.0.1.116',
 
 class DrivingRobot(object):
 
-    def __init__(self, hedgenumber, ip):
-        self.number = hedgenumber
+    def __init__(self, number, ip, final_x, final_y, alpha=45, epsilon=4):
+        self.number = number
         self.ip = ip
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.going_left = True
+        self.going_right = True
+        self.on_track = 0
+        self.fx = final_x
+        self.fy = final_y
+        self.done = False
+
+    def robot_start(self):
         self.addr, self.x0, self.y0, self.z0, self.t0 = hedgeswarm.number_position(self.number)
+        self.m, self.b = cm3.set_line(self.x0, self.y0, self.fx, self.fy)
 
-    def set_line(self, x0, y0, fx, fy):
-        slope = (fy - y0) / (fx - x0)
-        intercept = y0 - (x0 * slope)
-        return slope, intercept
-
-    def xexp(self, m, b, y):
-        ex = (y - b) / m
+    def xexp(self, y):
+        ex = (y - self.b) / self.m
         return ex
 
-    def turn(self, endx, endy):
+    def move(self, direction, value=None):
+
+        if direction == "turn":
+            payload = {'TURN': str(value)}
+        elif direction == "forward":
+            payload = {'FORWARD': str(value)}
+        elif direction == "stop":
+            payload = {'STOP': 'ON'}
+        elif direction == "backward":
+            payload = {'BACKWARD': 'ON'}
+        else:
+            print ("ERROR: unknown direction:", direction)
+
+        try:
+            headers = {'User-Agent': "Car"}
+            r = requests.get('http://' + self.ip, headers=headers, params=payload)
+        except Exception as e:
+            print(type(e), e)
+
+    def turn(self):
+        a, cx, cy, cz, ct = hedgeswarm.number_position(self.number)
+        ex = self.xexp(cy)
+        if abs(cx - self.fx) < self.epsilon and abs(cy - self.fy) < self.epsilon:
+            self.done = True
+        if (cx - ex) < - self.epsilon:
+            if self.going_left:
+                self.move('turn', self.alpha)
+                self.going_left = True
+                self.going_right = False
+                self.on_track += 1
+                if self.on_track > 2:
+                    self.alpha -= 5
+            elif not self.going_left:
+                self.alpha += 10
+                self.move('turn', self.alpha)
+                self.on_track = 0
+        elif (cx - ex) > self.epsilon:
+            if self.going_right:
+                self.move('turn', - self.alpha)
+                self.going_left = False
+                self.going_right = True
+                self.on_track += 1
+                if self.on_track > 2:
+                    self.alpha -= 5
+            elif not self.going_right:
+                self.alpha += 10
+                self.move('turn', self.alpha)
+                self.on_track = 0
+        time.sleep(.2)
+
+    def run(self):
+        while not self.done:
+            self.move('forward', 1)
+            time.sleep(.2)
+            self.turn()
+
+
 
 
