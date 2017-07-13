860fe690a2d2aec9044893e847a5bdd528fdc2d6
Gregor von Laszewski <laszewski@gmail.com>
2017-07-13 16:21:48 -0500

    new:dev: imporve setup document

--- bin/osx/setup.sh
+++ bin/osx/setup.sh
@@ -1,5 +1,12 @@
 # !/bin/sh
+xcode-select --install
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+brew install pyenv pyenv-virtualenv pyenv-virtualenvwrapper
+brew install readline xz
+
 
 pip install pyserial
 brew tap mengbo/ch340g-ch34g-ch34x-mac-os-x-driver https://github.com/mengbo/ch340g-ch34g-ch34x-mac-os-x-driver
@@ -12,9 +19,13 @@ brew install wget
 brew install libusb
 brew install mosquitto
 
+brew cask install aquamacs
 brew cask install adafruit-arduino
 brew cask install pycharm-ce
 
 pip install numpy
 pip install matplotlib
 echo "backend : TkAgg" > ~/.matplotlib/matplotlibrc
+
+pyenv install 3.6.1
+pyenv virtualenv 3.6.1 ENV3
