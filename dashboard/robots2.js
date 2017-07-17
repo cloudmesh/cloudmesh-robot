954a0b3fef5604743729504be857f27406a65424
Local Administrator <localadmin@ISELAB-MAC-01.local>
2017-07-17 17:30:51 -0400

    new:dev: added kids' robots to doashboard

---
+++ dashboard/robots2.js
@@ -0,0 +1,89 @@
+robots = {
+    robots: [
+        {
+            name: 'Whipped Cream',
+            chipid: '0x000eafac',
+            mac: '2c:3a:e8:0e:af:ac',
+            ip: '10.0.1.221'
+        },
+        {
+            name: 'Georgie',
+            chipid: '0x00192ce5',
+            mac: 'a0:20:a6:19:2c:e5',
+            ip: '10.0.1.112'
+        },
+        {
+            name: 'Dragon',
+            chipid: '0x000f22ab',
+            mac: '2c:3a:e8:0f:22:ab',
+            ip: '10.0.1.222'
+        },
+        {
+            name: 'Reginald',
+            chipid: '0x00dc7fce',
+            mac: '5c:cf:7f:dc:7f:ce',
+            ip: '10.0.1.107'
+        },
+        {
+            name: 'Bot',
+            chipid: '0x000bd79e',
+            mac: '60:01:94:0b:d7:9e',
+            ip: '10.0.1.111'
+        },
+        {
+            name: 'Siri',
+            chipid: '0x000a5bfc',
+            mac: 'a0:20:a6:0a:5b:fc',
+            ip: '10.0.1.109'
+        },
+        {
+            name: 'Dragon Slayer',
+            chipid: '0x000f21f3',
+            mac: '2c:3a:e8:0f:21:f3',
+            ip: '10.0.1.223'
+        },
+        {
+            name: 'Santa',
+            chipid: '0x0016051a',
+            mac: 'a0:20:a6:16:05:1a',
+            ip: '10.0.1.224'
+        },
+        {
+            name: 'Robot',
+            chipid: '0x000f2257',
+            mac: '2c:3a:e8:0f:22:57',
+            ip: '10.0.1.225'
+        },
+        {
+            name: 'Walli',
+            chipid: '0x000eb654',
+            mac: '2c:3a:e8:0e:b6:54',
+            ip: '10.0.1.226'
+        },
+        {
+            name: 'Larri',
+            chipid: '0x000eaea4',
+            mac: '2c:3a:e8:0e:ae:a4',
+            ip: '10.0.1.227'
+        },
+        {
+            name: 'Larry',
+            chipid: '0x000bd802',
+            mac: '60:01:94:0b:d8:02',
+            ip: '10.0.1.110'
+        },
+        {
+            name: 'The Tank 0.9',
+            chipid: '0x00931848',
+            mac: '5c:cf:7f:93:18:48',
+            ip: '10.0.1.105'
+        },
+        {
+            name: 'Wise Puppet',
+            chipid: '0x00dc7e41',
+            mac: '5c:cf:7f:dc:7e:41',
+            ip: '10.0.1.108'
+        }
+
+    ]
+}
