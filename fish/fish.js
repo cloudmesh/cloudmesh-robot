3d9308e61b8ab32700a997ccd9e8258fa13ad205
Jon <jon@ISELAB-MAC-14.local>
2017-07-20 10:40:02 -0400

    new:dev: updated fish inventory

--- fish/fish.js
+++ fish/fish.js
@@ -5,49 +5,43 @@ fish = {
             ip: '10.0.1.101'
         },
         {
-            name: 'fishi30',
+            name: 'fishi30', //checked
             chipid: '0x00174533',         
             mac: 'a0:20:a6:17:45:33',       
             ip: '10.0.1.130'
         },
         {
-            name: 'fishi31',
-            chipid: '0x001c02eb',              
-            mac: 'a0:20:a6:1c:02:eb',       
-            ip: '10.0.1.131'
-        },
-        {
-            name: 'fishi32',
+            name: 'fishi32', //checked
             chipid: '0x001c04a9',              
             mac: 'a0:20:a6:1c:04:a9',       
             ip: '10.0.1.132' 
         },
         {
-            name: 'fishi33',
-            chipid: 0x0015fe2f,              
+            name: 'fishi33', //checked
+            chipid: '0x0015fe2f',
             mac: 'a0:20:a6:15:fe:2f',       
             ip: '10.0.1.133'
         },
         {
-            name: 'fishi34',
+            name: 'fishi34', //checked
             chipid: '0x001c019f',              
             mac: 'a0:20:a6:1c:01:9f',       
             ip: '10.0.1.134' 
         },
         {
-            name: 'fishi35',
+            name: 'fishi35', //checked
             chipid: '0x0017481c',              
             mac: 'a0:20:a6:17:48:1c',       
             ip:  '10.0.1.135'
         },
         {
-            name: 'fishi36',
+            name: 'fishi36', //checked
             chipid: '0x0015d8a7',              
             mac: 'a0:20:a6:15:d8:a7',       
             ip:  '10.0.1.136'
         },
         {
-            name: 'fishi37',
+            name: 'fishi37', //checked
             chipid: '0x001601ac',              
             mac: 'a0:20:a6:16:01:ac',       
             ip:  '10.0.1.137'
@@ -63,6 +57,24 @@ fish = {
             chipid: '0x00f054ae',              
             mac: '5c:cf:7f:f0:54:ae',       
             ip:  '10.0.1.139'
+        },
+        {
+            name: 'fishi42', //checked
+            chipid: '0x00d1089c',
+            mac: '5c:cf:7f:d1:08:9c',
+            ip:  '10.0.1.142'
+        },
+        {
+            name: 'fishi43', //checked
+            chipid: '0x0016051a',
+            mac: 'a0:20:a6:16:05:1a',
+            ip:  '10.0.1.143'
+        },
+        {
+            name: 'fishi44', //checked
+            chipid: '0x000ebde1',
+            mac: '2c:3a:e8:0e:bd:e1',
+            ip:  '10.0.1.144'
         }
     ]
 }
